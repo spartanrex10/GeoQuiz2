ddb225d42af681e4e106bf35012668644501466e
spartanrex10 <46965967+spartanrex10@users.noreply.github.com>
2019-02-28 09:01:52 -0600

    Added an "On Click Listener" to both buttons.

--- app/src/main/java/com/ctech/zachary/geoquiz/MainActivity.java
+++ app/src/main/java/com/ctech/zachary/geoquiz/MainActivity.java
@@ -2,16 +2,33 @@ package com.ctech.zachary.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button true_button;
-    private Button false_button;
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Does nothing yet.
+            }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Also does nothing yet.
+            }
+        });
     }
 }
