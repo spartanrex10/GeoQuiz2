63c19a19714af6ccbaf29def3108f30632be08fa
spartanrex10 <46965967+spartanrex10@users.noreply.github.com>
2019-03-04 08:16:50 -0600

    Made it so that the 'on click listener' actually does something. That something being that it will say correct or incorrect.

--- app/src/main/java/com/ctech/zachary/geoquiz/MainActivity.java
+++ app/src/main/java/com/ctech/zachary/geoquiz/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,13 +22,17 @@ public class MainActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Does nothing yet.
+                Toast.makeText(MainActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
             }
         });
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Also does nothing yet.
+                Toast.makeText(MainActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
