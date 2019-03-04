9243e48bebb71e2ca70a192508942d00c849f9f6
spartanrex10 <46965967+spartanrex10@users.noreply.github.com>
2019-03-04 08:51:20 -0600

    Made it so the 'Toast' appears at the top of the screen instead of the bottom.

--- app/src/main/java/com/ctech/zachary/geoquiz/MainActivity.java
+++ app/src/main/java/com/ctech/zachary/geoquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.ctech.zachary.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,17 +23,19 @@ public class MainActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                Toast toast= Toast.makeText(MainActivity.this,
+                        R.string.correct_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 150);
+                toast.show();
             }
         });
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                Toast toast= Toast.makeText(MainActivity.this,
+                        R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 150);
+                toast.show();
             }
         });
     }
